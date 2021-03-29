8277c4ea1c7ab64b0588591009e429b23d2d4383
Troy LoBue <troylobue33@gmail.com>
2021-03-29 16:00:43 -0500

    changes to the script

--- area_of_rectangle.py
+++ area_of_rectangle.py
@@ -31,6 +31,7 @@ def area_of_rectangle(height, width = None):
     """
     if width:
         width = height
+    import pdb; pdb.set_trace()
     area = height * width
     return area
 
@@ -41,11 +42,11 @@ if __name__ == '__main__':
                 "\tthe height of a square or the height and width of a "
                 "rectangle".format(script_name = sys.argv[0]))
         sys.exit(message)
-    height = sys.argv[1]
+    height = int(sys.argv[1])
     width = height
     if len(sys.argv) > 3:
-        width = sys.argv[1]
-
+        width = int(sys.argv[1])
+    
     area = area_of_rectangle(height, width)
 
     message = "The area of a {h} X {w} rectangle is {a}".format(
